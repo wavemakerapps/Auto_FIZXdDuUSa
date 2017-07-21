de9b7d04bd584da6948f2ce11c7d2b6f5ca0d09a
root <root@61949b3d2f95>
2017-07-21 06:19:33 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_FIZXdDuUSa",
+  "homePage" : "Main",
+  "name" : "Auto_FIZXdDuUSa",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
